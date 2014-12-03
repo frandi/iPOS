535f1fa477af19c4356e2d964a9a5657a6856418
Frandi <dev@rumahfrandi.com>
2014-12-03 12:13:05 +0700

    Added web api project

---
+++ iPOS.WebAPI/Controllers/POSController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace iPOS.WebAPI.Controllers
+{
+    [RoutePrefix("api/POS")]
+    public class POSController : ApiController
+    {
+        [Authorize]
+        [Route("Products")]
+        public IHttpActionResult Get()
+        {
+            return Ok(POS.GetProducts());
+        }
+    }
+
+    #region Helpers
+
+    public class POS
+    {
+        public static List<string> GetProducts()
+        {
+            return new List<string>() { "TV", "SmartPhone", "Radio", "DVD Player" };
+        }
+    }
+
+    #endregion
+}
---
+++ iPOS.WebAPI/Data/AuthRepository.cs
@@ -0,0 +1,50 @@
+﻿using iPOS.WebAPI.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace iPOS.WebAPI.Data
+{
+    public class AuthRepository : IDisposable
+    {
+        private AuthContext _context;
+
+        private UserManager<IdentityUser> _userManager;
+
+        public AuthRepository()
+        {
+            _context = new AuthContext();
+            _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_context));
+        }
+
+        public async Task<IdentityResult> RegisterUser(User user)
+        {
+            IdentityUser iuser = new IdentityUser
+            {
+                UserName = user.UserName
+            };
+
+            var result = await _userManager.CreateAsync(iuser, user.Password);
+
+            return result;
+        }
+
+        public async Task<IdentityUser> FindUser(string userName, string password)
+        {
+            IdentityUser iuser = await _userManager.FindAsync(userName, password);
+
+            return iuser;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            _userManager.Dispose();
+
+        }
+    }
+}
